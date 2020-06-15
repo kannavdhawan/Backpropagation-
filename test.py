9062f8417622c356b42f78fa9baf6e7f9fd744af
Kannav Dhawan <30934308+kannavdhawan@users.noreply.github.com>
2020-06-15 02:57:06 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from test_mlp import test_mlp, STUDENT_NAME, STUDENT_ID
 from acc_calc import accuracy
 import pandas as pd 
-y_pred = test_mlp('./sample.csv')
-test_labels=pd.read_csv("labels.csv",header=None).to_numpy()
-test_accuracy= accuracy(test_labels, y_pred)*100
-print(test_accuracy)
+import sys
+def main(path_file):
+    # path_file='./sample.csv'
+    y_pred = test_mlp(path_file)
+    test_labels=pd.read_csv("labels.csv",header=None).to_numpy()
+    test_accuracy= accuracy(test_labels, y_pred)*100
+    print(test_accuracy)
+if __name__=='__main__':
+    main(sys.argv[1])
